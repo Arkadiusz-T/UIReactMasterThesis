d4b96e33bd1c87dfe4b8a4facfc413e8cb1e1c72
Your Name <you@example.com>
2023-01-31 00:22:12 +0100

    added implementation of soap post request

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {useState, useRef} from 'react';
 import InfoBar from "./components/InfoBar";
 import ChoseTestParameters from "./components/ChoseTestParameters";
+import axios from 'axios';
 
 function App() {
   const timePreFetch = useRef();
@@ -12,7 +13,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [err, setErr] = useState('');
   
-  const handleClick = async () => {
+  const sendRestRequest = async () => {
     setIsLoading(true);
 
     try {
@@ -48,13 +49,67 @@ function App() {
     }
   };
 
+  const sendSoapRequest = async () => {
+    setIsLoading(true);
+
+    try {
+      var dbmsType = document.getElementById("dbmsSelect").value;
+      var textLength = document.getElementById("dlugoscTekstuSelect").value;
+      var textType = document.getElementById("typZmiennejSelect").value;
+      const url = `http://localhost:8080/ws`;
+      timePreFetch.current = (new Date()).getTime();
+      const xmlEnvelope = `
+      <?xml version="1.0" encoding="utf-8"?>
+      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+                      xmlns:std="http://www.dupa.pl/soapexample"
+      >
+        <soap:Body>
+          <std:getSoapTimes >
+            <textLength>${textLength}</textLength>
+            <textType>${textType}</textType>
+            <czasWyslaniaRequestuZFrontendu>${timePreFetch.current}</czasWyslaniaRequestuZFrontendu>
+            <dbmsType>${dbmsType}</dbmsType>
+          </std:getSoapTimes>
+        </soap:Body>
+      </soap:Envelope>
+    `;
+
+      const response = await axios.post(
+      'https://www.w3schools.com/xml/tempconvert.asmx?wsdl',
+      xmlEnvelope,
+      {
+        headers: {
+          'Content-Type': 'text/xml',
+        },
+      },
+    );
+
+      timePostFetch.current = (new Date()).getTime();
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
    var timeBackToFront = timePostFetch.current - data.czasWystawieniaOdpowiedziDlaFrontendu
 
   return (
     <div className="frontendView">
       <InfoBar />
       <ChoseTestParameters parametersFor="Text parameters" />
-      <button onClick={handleClick}> Compare texts function</button>
+      <button onClick={sendRestRequest}> Compare texts REST</button>
+      <button onClick={sendSoapRequest}> Compare texts SOAP</button>
       {!isLoading && <div>
             <h1>Results</h1>
             <div>
